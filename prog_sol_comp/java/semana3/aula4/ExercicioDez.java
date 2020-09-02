b56f9b63e698ff674110f558b1844d64ba1c9b94
Arth-Monteiro <arthur.m.monteiro2902@gmail.com>
2020-09-02 20:11:44 -0300

    Exercicio 10

---
+++ prog_sol_comp/java/semana3/aula4/ExercicioDez.java
@@ -0,0 +1,14 @@
+package semana3.aula4;
+
+public class ExercicioDez {
+    
+    public static void main(String[] args) {
+        
+        // 10. Fazer um programa que recebe o preço de um produto, calcula e mostra
+        // o novo preço sabendo-se que este sofreu um desconto de 10%.
+
+        System.out.println("Hello World");
+
+    }
+
+}
