2292ebbb2717bc0ae106f02c7c0a0c6064c2db94
Arth-Monteiro <arthur.m.monteiro2902@gmail.com>
2020-09-01 22:35:41 -0300

    Exercicio Nove aula de Programacao de Solucoes Computacionais semana 3

--- prog_sol_comp/java/semana1/aula1/HelloWorld.java
+++ prog_sol_comp/java/semana1/aula1/HelloWorld.java
@@ -1,3 +1,5 @@
+package semana1.aula1;
+
 public class HelloWorld {
     public static void main (String[] args) {
         String s = "Hello World!";
---
+++ prog_sol_comp/java/semana3/aula4/ExercicioNove.java
@@ -0,0 +1,41 @@
+package semana3.aula4;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class ExercicioNove {
+
+    public static void main(String[] args) {
+        
+        // 9. Fazer um programa que recebe um número positivo, calcula e mostra:
+        // a) O número digitado ao quadrado;
+        // b) O número digitado ao cubo;
+        // c) A raiz quadrada do número digitado.
+
+        Scanner sc = new Scanner(System.in);
+        double num;
+
+        while (true) {
+            
+            System.err.print("Digite um numero positivo: ");
+            num = sc.nextDouble();
+            
+            if (num >= 0) {
+
+                sc.close();
+                break;
+
+            }
+
+        }
+
+        double quadrado = Math.pow(num, 2);
+        double cubo = Math.pow(num, 3);
+        double raiz = Math.sqrt(num);
+
+        System.out.println("Quadrado: " + quadrado);
+        System.out.println("Cubo: " + cubo);
+        System.out.println("Raiz Quadrada: " + raiz);
+
+    }
+}
